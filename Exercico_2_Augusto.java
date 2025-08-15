f01409aa54341baf773678c0379d9038e3982bc8
Augusto Luiz Marim <augustolm06@gmail.com>
2025-08-15 08:35:06 -0300

    Add files via upload

---
+++ Exercico_2_Augusto.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alunodev09
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Exercícios IF ELSE
+        
+        //Exercício 1
+        
+        /*System.out.println("Insira um número!");
+        int numero = scanner.nextInt();
+        if(numero %2 == 0){
+            System.out.println("Este número é PAR!");
+        }
+        else{
+            System.out.println("Este número é IMPAR!");
+        }*/
+        
+        //Exercício 2
+        
+        /*System.out.println("Insira sua idade!");
+        int idade = scanner.nextInt();
+        if(idade >= 18){
+            System.out.println("Você é maior de idade!");
+        }
+        else{
+            System.out.println("Você é menor de idade!");
+        }*/
+        
+        //Exercício 3
+        
+        /*System.out.println("Insira seu salário!");
+        float salario = scanner.nextFloat();
+        if(salario <= 1000.00){
+            salario *= 1.1;
+            System.out.println("Seu salário com bônus de 10%!" + salario);
+        }
+        else{
+            salario *= 1.05;
+            System.out.println("Seu salário com bônus de 5%" + salario);
+        }*/
+    }
+}
